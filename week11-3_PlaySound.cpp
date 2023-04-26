1665c9cd7d4e755288a2f072e9557ee4d9cffe41
WU-BING-SYUN <ntuwualio@gmail.com>
2023-04-26 12:02:40 +0800

    week11

---
+++ week11-3_PlaySound.cpp
@@ -0,0 +1,11 @@
+#include <windows.h>
+
+int main()
+{
+
+    PlaySound("do_re_mi\\do.wav", NULL, SND_SYNC);
+    PlaySound("do_re_mi\\do.wav", NULL, SND_SYNC);
+    PlaySound("do_re_mi\\do.wav", NULL, SND_SYNC);
+    PlaySound("do_re_mi\\re.wav", NULL, SND_SYNC);
+    PlaySound("do_re_mi\\mi.wav", NULL, SND_SYNC);
+}
