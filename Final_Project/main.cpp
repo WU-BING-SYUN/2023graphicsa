5c94804b0f414e8aec42ec98963276a52ae39096
WU-BING-SYUN <ntuwualio@gmail.com>
2023-05-17 12:23:07 +0800

    week14

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -7,7 +7,7 @@ GLMmodel * body = NULL;
 GLMmodel * uparmR = NULL;
 GLMmodel * lowarmR = NULL;
 int show[4] = {1,1,1,1};
-int ID = 0;
+int ID = 3;
 void keyboard(unsigned char key, int x, int y)
 {
     if(key=='0') ID = 0;
@@ -50,16 +50,33 @@ void display()
         if(show[1]) glmDraw(body, GLM_MATERIAL);
 
         glPushMatrix();
-            glTranslatef(teapotX, teapotY, 0);
+            glTranslatef(+1.240000, +0.600000, 0);
+            glRotatef(angle,0,0, 1);
+            glTranslatef(-1.240000, -0.600000, 0);
+            //glTranslatef(teapotX,teapotY,0);
+
             if(ID==2) glColor3f(1,0,0);
             else glColor3f(1,1,1);
             if(show[2]) glmDraw(uparmR, GLM_MATERIAL);
+
+            glPushMatrix();
+
+               glTranslatef(+1.899999, +0.126667, 0);
+               glRotatef(angle,0,0, 1);
+               glTranslatef(-1.899999, -0.126667, 0);
+               //glTranslatef(teapotX,teapotY,0);
+
+               if(ID==3) glColor3f(1,0,0);
+               else glColor3f(1,1,1);
+               if(show[3]) glmDraw(lowarmR, GLM_MATERIAL);
+
+            glPopMatrix();
         glPopMatrix();
 
-        if(ID==3) glColor3f(1,0,0);
-        else glColor3f(1,1,1);
-        if(show[3]) glmDraw(lowarmR, GLM_MATERIAL);
+
     glPopMatrix();
+    glColor3f(0,1,0);
+    glutSolidTeapot(0.02);
     glutSwapBuffers();
 }
 int oldX = 0, oldY = 0;
@@ -69,6 +86,7 @@ void motion(int x, int y)
     teapotY -= (y - oldY)/150.0;
     oldX = x;
     oldY = y;
+    angle = x;
     printf("glTranslatef(%f, %f, 0);\n", teapotX, teapotY);
     glutPostRedisplay();
 }
@@ -76,7 +94,8 @@ void mouse(int button, int state, int x, int y)
 {
     if(state==GLUT_DOWN){
         oldX = x;
-        oldY = y;///teapotX = (x-150)/150.0;
+        oldY = y;
+        ///teapotX = (x-150)/150.0;
         ///teapotY = (150-y)/150.0;
         angle = x;
         ///printf("glTranslatef(%f, %f, 0);\n", teapotX, teapotY);
