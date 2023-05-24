9e7695de3686c9e4eefab5585025ed734eca7da4
WU-BING-SYUN <ntuwualio@gmail.com>
2023-05-24 12:03:18 +0800

    week15

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -8,19 +8,35 @@ GLMmodel * uparmR = NULL;
 GLMmodel * lowarmR = NULL;
 int show[4] = {1,1,1,1};
 int ID = 3;
+FILE * fout = NULL; ///step02-2 一開始,檔案沒有開, NULL
+FILE * fin = NULL; ///step02-2 要讀檔用的指標, 一開始也是 NULL
+float teapotX=0, teapotY=0; ///幫我們看移動值
+///float angle=0, angle2=0, angle3=0;///step03-2 擺動作
+float angle[20] = {};
 void keyboard(unsigned char key, int x, int y)
 {
     if(key=='0') ID = 0;
     if(key=='1') ID = 1;
     if(key=='2') ID = 2;
     if(key=='3') ID = 3;
+    if(key=='s')
+    {
+        if(fout==NULL) fout = fopen("motion.txt", "w");
+        for(int i = 0; i < 20; i++)
+        {
+            fprintf(fout, "%.2f ", angle[i]);
+        }
+        fprintf(fout, "\n");
+    }else if(key=='r'){
+        if(fin==NULL) fin = fopen("motion.txt", "r");
+        for(int i = 0; i < 20; i++)
+        {
+            fscanf(fin, "%f", &angle[i]);
+        }
+    }
     glutPostRedisplay();
 }
 
-FILE * fout = NULL; ///step02-2 一開始,檔案沒有開, NULL
-FILE * fin = NULL; ///step02-2 要讀檔用的指標, 一開始也是 NULL
-float teapotX=0, teapotY=0; ///幫我們看移動值
-float angle=0, angle2=0, angle3=0;///step03-2 擺動作
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
@@ -51,7 +67,7 @@ void display()
 
         glPushMatrix();
             glTranslatef(+1.240000, +0.600000, 0);
-            glRotatef(angle,0,0, 1);
+            glRotatef(angle[2],0,0, 1);
             glTranslatef(-1.240000, -0.600000, 0);
             //glTranslatef(teapotX,teapotY,0);
 
@@ -62,7 +78,7 @@ void display()
             glPushMatrix();
 
                glTranslatef(+1.899999, +0.126667, 0);
-               glRotatef(angle,0,0, 1);
+               glRotatef(angle[3],0,0, 1);
                glTranslatef(-1.899999, -0.126667, 0);
                //glTranslatef(teapotX,teapotY,0);
 
@@ -84,9 +100,10 @@ void motion(int x, int y)
 {
     teapotX += (x - oldX)/150.0;
     teapotY -= (y - oldY)/150.0;
+    angle[ID] += (x - oldX);
     oldX = x;
     oldY = y;
-    angle = x;
+    ///angle = x;
     printf("glTranslatef(%f, %f, 0);\n", teapotX, teapotY);
     glutPostRedisplay();
 }
@@ -97,7 +114,7 @@ void mouse(int button, int state, int x, int y)
         oldY = y;
         ///teapotX = (x-150)/150.0;
         ///teapotY = (150-y)/150.0;
-        angle = x;
+        ///angle = x;
         ///printf("glTranslatef(%f, %f, 0);\n", teapotX, teapotY);
         ///if(fout==NULL) fout = fopen("file4.txt", "w"); ///step02-2 沒開檔,就開
         ///fprintf(fout, "%f %f\n", teapotX, teapotY); ///step02-2 要再存座標
